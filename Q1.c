4d92527b8050a022b19a36b01cb2616d688b21f2
Riddhi Gupta <guptariddhi2007@gmail.com>
2025-09-11 00:52:47 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter first no.");
+    scanf("%d", &a);
+    printf(" \n enter second no.");
+    scanf("%d", &b);
+    int c= a + b;
+
+
+    printf("sum of a & b is : %d", a + b );
+
+    return 0;
+}
